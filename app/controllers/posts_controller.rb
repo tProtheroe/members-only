a52f05ba5818fc28d0b72ee98498df2526ed3a4c
Tomos Protheroe <tomos@craftcourses.com>
2022-01-27 17:48:45 +0000

    Remove comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/new
   def new
     @post = current_user.posts.build
   end
@@ -37,7 +35,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
 
@@ -47,13 +44,12 @@ class PostsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
+  private 
+
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(
         :post
